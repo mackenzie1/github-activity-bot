6907041d6af7f6629fa1e7387245dba2e4c75d53
mackenzie1 <mackenzie.harwood1@gmail.com>
2023-06-12 18:30:05 -0400

    add steps

--- github_bot.py
+++ github_bot.py
@@ -25,7 +25,7 @@ driver.get("https://github.com/login")
 inp_username = driver.find_element(By.XPATH, "//input[@id='login_field']")
 
 inp_username.send_keys(username)
-inp_username.send_keys(Keys.ENTER)
+# inp_username.send_keys(Keys.ENTER)
 
 inp_password = driver.find_element(By.XPATH, "//input[@id='password']")
 
@@ -34,27 +34,37 @@ inp_password.send_keys(Keys.ENTER)
 
 time.sleep(2)
 
-submit_btn = driver.find_element(By.XPATH, "//body/div[1]/div[3]/main[1]/div[1]/div[4]/form[1]/div[1]/input[13]")
+# submit_btn = driver.find_element(By.XPATH, "//body/div[1]/div[3]/main[1]/div[1]/div[4]/form[1]/div[1]/input[13]")
 
-element = WebDriverWait(driver, 20).until(
-EC.element_to_be_clickable((submit_btn)))
-
-
-
-
+# element = WebDriverWait(driver, 20).until(
+# EC.element_to_be_clickable((submit_btn)))
 time.sleep(2)
 
 
+repoSearch = "github-activity-bot"
+
+inp_repo_search = driver.find_element(By.XPATH, "//input[@id='dashboard-repos-filter-left']")
+
+time.sleep(2)
+
+inp_repo_search.send_keys(repoSearch)
+inp_repo_search.send_keys(Keys.ENTER)
+
+time.sleep(2)
+'//a[contains(@href, "WO20")]'
+findRepoByLinktxt =  driver.find_element(By.XPATH, '//a[contains(@href, "github-activity-bot")]')
+# findRepoByLinktxt =  driver.find_element(By.PARTIAL_LINK_TEXT("github-activity-bot"))
+time.sleep(2)
+time.sleep(2)
+
+findRepoByLinktxt.click()
+
+time.sleep(2)
+time.sleep(2)
 # selected_contact = driver.find_element("//span[@title='"+contact+"']")
 # selected_contact.click()
 
 # textBox = driver.find_element("xpath", "//input[@title='Type a message]")
 # textBox2 = "//body/div[@id='app']/div[1]/div[1]/div[5]/div[1]/footer[1]/div[1]/div[1]/span[2]/div[1]/div[2]/div[1]/div[1]/div[1]"
-
-# inp_xpath = '//div[@class="_2S1VP copyable-text selectable-text"][@contenteditable="true"][@data-tab="1"]'
-# input_box = driver.find_element(inp_xpath)
-# time.sleep(2)
-# textBox.send_keys(text + Keys.ENTER)
-# time.sleep(2)
 
 driver.quit()
