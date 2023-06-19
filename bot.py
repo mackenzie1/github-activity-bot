4e40f1f4bc2e4c90ee54a0c0756e173aa40ec626
mackenzie1 <mackenzie.harwood1@gmail.com>
2023-06-19 13:07:24 -0400

    working script

--- bot.py
+++ bot.py
@@ -1,9 +1,8 @@
+from hashlib import sha1, sha512
 import requests
 from pprint import pprint
 import base64
 from github import Github
-# Authentication is defined via github.Auth
-# from github import Auth
 import os
 
 message = "Hi Mackenzie"
@@ -11,10 +10,9 @@ print (message)
 
 username = "mackenzie1"
 
-key = "GITHUB_PASSWORD"
-password = os.getenv(key, default=None)
+#token in env variables
+token = os.environ.get('GH_TOKEN')
 
-# auth = Auth.Login(username, password)
 
 # url to request
 url = f"https://api.github.com/users/{username}"
@@ -22,48 +20,41 @@ url = f"https://api.github.com/users/{username}"
 # make the request and return the json
 user_data = requests.get(url).json()
 
-# pretty print JSON data
+# # # pretty print JSON data
 pprint(user_data)
 
-# g = Github(auth=auth)
 
-g = Github( )
+# using an access token
+g = Github(login_or_token=token)
 
 # login
 g.get_user().login
 
 user = g.get_user()
 
+print(user.name)
+
 #print all the repos
 for repo in user.get_repos():
     print(repo)
 
-#search for name/github-activity-bot
-repo = g.search_repositories("{username}/github-activity-bot")[0]
+repoName = "mackenzie1/github-activity-bot"
 
-# confirmedRepo = g.get_repo(repo)
+repo = g.get_repo(repoName)
+
+
+print(repo.name)
 
 #get readme contents and print
+contents2 = repo.get_contents("bot.py")
+print(contents2)
+
+
 contents = repo.get_contents("README.md")
 print(contents)
 
-#update read me with some text
-# repo.update_file(contents.path, "github activity bot", "bot activity..", contents.sha, branch="main")
+repo.create_file("test2.txt", "test", "test", branch="test")
 
-# # create a file and commit n push
-repo.create_file("test.txt", "commit message", "content of the file")
+contents = repo.get_contents("test.txt", ref="test")
 
-
-# contents = repo.get_contents("test.txt", ref="test")
-# repo.update_file(contents.path, "more tests", "more tests", contents.sha, branch="test")
-
-# def main():
-# 	args = get_args()
-# 	g = Github(args.name,args.password)
-# 	opt = args.option
-# 	if (opt == "branch"):
-# 		brancher(args.query, g)
-# 	elif (opt == "star"):
-# 		starrer(args.query, g)
-# 	else:
-# 		print("Possible options (-o): 'branch' or 'star'.")
+print(contents)
